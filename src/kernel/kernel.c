7eb275cfe8a0152696c444957da78df733eadcc4
OkulusDev <x.okulus.dev@gmail.com>
2023-10-31 23:41:36 +0700

    simple edit codstyle, docs, readme

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -18,32 +18,30 @@
 #include "../libc/mem.h"
 
 
-void	kmain() {
+void kmain() {
 	// Запускаемая функция ядра //
 	clear_screen();
 	isr_install();
     irq_install();
 
-	// clear_screen();
-
-	kprint("Welcome to Metalfish OS 0.9.11\n");
+	kprint("Welcome to Metalfish OS 0.10.12\n");
 	kprint("Repository: https://github.com/OkulusDev/Metalfish-OS\n\n");
 
-	kprint("MetalShell v0.1.0\n"
+	kprint("MetalShell v0.2.3\n"
 	        "Type END to halt the CPU\n"
 	        "Type HELP to view commands\n!#> ");
 }
 	
 void user_input(char *input) {
     if (strcmp(input, "END") == 0) {
-        kprint("Stopping the CPU. Bye!\n");
+        kprint("Stopping the CPU. Bye! (Shutdown PC)\n");
         asm volatile("hlt");
     } else if (strcmp(input, "HELP") == 0) {
     	kprint("END - stopping the CPU\n"
     			"INFO - info about OS\n"
     			"PAGE - to request a kmalloc()");
     } else if (strcmp(input, "INFO") == 0) {
-    	kprint("Metalfish OS 0.9.11 by OkulusDev\n"
+    	kprint("Metalfish OS 0.10.12 by OkulusDev\n"
     			"Native Build, Release Beta 2\n"
     			"Hello World!\n");
     }  else if (strcmp(input, "PAGE") == 0) {
