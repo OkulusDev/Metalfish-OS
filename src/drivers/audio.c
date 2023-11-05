d6081448ab8e2e87e6cf6ad84fa7f1f57ed575b4
OkulusDev <x.okulus.dev@gmail.com>
2023-11-05 19:28:09 +0700

    add audio drivers, commenting code, small changes

--- src/drivers/audio.c
+++ src/drivers/audio.c
@@ -1,21 +1,18 @@
 #include "audio.h"
 
-#
 
-// Does not work.
+// Возможно не работает
 
-//Play sound using built in speaker
+// Звук при помощи внутренних устройств
 void play_sound(u32 nFrequence) {
         u32 Div;
         u8 tmp;
 
-        // Set the PIT to the desired frequency.
         Div = 1193180 / nFrequence;
         port_byte_out(0x43, 0xb6);
         port_byte_out(0x42, (u8) (Div) );
         port_byte_out(0x42, (u8) (Div >> 8));
 
-        // Play the sound using the PC speaker.
         tmp = port_byte_in(0x61);
         if (tmp != (tmp | 3)) {
                 port_byte_out(0x61, tmp | 3);
@@ -23,13 +20,11 @@ void play_sound(u32 nFrequence) {
 
 }
 
-// Make it shutup.
 void nosound() {
         u8 tmp = port_byte_in(0x61) & 0xFC;
         port_byte_out(0x61, tmp);
 }
 
-// Make a beep.
 void beep() {
         play_sound(1000);
         nosound();
--- src/drivers/audio.h
+++ src/drivers/audio.h
@@ -1,7 +1,6 @@
 #include "../cpu/type.h"
 #include "../cpu/ports.h"
 
-// Does not work.
 void play_sound(u32 nFrequence);
 void nosound();
 void beep();
